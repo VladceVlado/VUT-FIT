5473aeaafe78b17cdbf1ea5da1ccf501f47acc4c
Vlada Dusek <v.dusek96@gmail.com>
2019-06-14 00:22:31 +0200

    Add ITW2

---
+++ sem6/ITW2/src/script.js
@@ -0,0 +1,9 @@
+function show_detail() {
+    let x = document.getElementById("js-trick");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
